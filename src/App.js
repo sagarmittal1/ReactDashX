75220d983b3d47c639e46dccf9355a25238d3a3c
Sagar Mittal <mittalsagar006@gmail.com>
2023-02-28 00:15:45 +0530

    made Topbar & make it functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
+import Topbar from './scenes/global/Topbar';
+
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -9,7 +11,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
