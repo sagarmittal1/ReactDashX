a11d4ad87047de3ecce01684f62c9734ce66fde6
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-03 23:04:58 +0530

    fix some errors

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  return <div>Dashboard</div>;
+};
 
-export default Dashboard
+export default Dashboard;
