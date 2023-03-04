3eaf36621638eefde66b28166a867a1739cfa623
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-04 22:27:38 +0530

    made invoices page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
-// import Invoices from './scenes/invoices';
+import Invoices from './scenes/invoices';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -32,7 +32,7 @@ function App() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                <Route path="/invoices" element={<Invoices />} />
                 {/* <Route path="/form" element={<Form />} /> */}
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
