5431b1037f5157172fb07aa873000b7b01070da3
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-08 23:52:32 +0530

    made form page using Formik & yup

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Sidebar from './scenes/global/Sidebar';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
+import Form from './scenes/form';
 // import Bar from './scenes/bar';
-// import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
@@ -33,7 +33,7 @@ function App() {
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
-                {/* <Route path="/form" element={<Form />} /> */}
+                <Route path="/form" element={<Form />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
