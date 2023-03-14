8d04e786b5e68a2b0c6c6eabf27f61868dc349c0
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-14 22:25:03 +0530

    made calendar page using FullCalendar library

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
+import Calendar from "./scenes/calendar/";
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
-// import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -34,12 +34,12 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
                 {/* <Route path="/faq" element={<FAQ />} /> */}
                 {/* <Route path="/geography" element={<Geography />} /> */}
-                {/* <Route path="/calendar" element={<Calendar />} /> */}
               </Routes>
             </main>
           </div>
