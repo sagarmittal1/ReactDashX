38c2d7b24353290acdeaec4b46480fecf0ec79ee
Sagar Mittal <mittalsagar006@gmail.com>
2023-03-15 02:19:57 +0530

    made faq using accordion component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
 import Calendar from "./scenes/calendar/";
+import FAQ from './scenes/faq';
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
-// import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
 
 function App() {
@@ -35,10 +35,10 @@ function App() {
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
                 <Route path="/calendar" element={<Calendar />} />
+                <Route path="/faq" element={<FAQ />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
-                {/* <Route path="/faq" element={<FAQ />} /> */}
                 {/* <Route path="/geography" element={<Geography />} /> */}
               </Routes>
             </main>
---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,47 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { tokens } from '../../theme';
+import Header from '../../components/Header';
+
+const faqData = [
+  {
+    summary: 'This is first question',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime distinctio fugiat dignissimos molestiae pariatur, consectetur aperiam, officiis incidunt, odit asperiores unde praesentium. Iusto maxime soluta molestiae in alias corrupti modi.',
+  },
+  {
+    summary: 'This is second question',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime distinctio fugiat dignissimos molestiae pariatur, consectetur aperiam, officiis incidunt, odit asperiores unde praesentium. Iusto maxime soluta molestiae in alias corrupti modi.',
+  },
+];
+
+const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="15px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions" />
+
+      {faqData.map((faq) => (
+        <Accordion
+          defaultExpanded
+          sx={{ backgroundColor: colors.primary[400], borderRadius: '4px' }}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography color={colors.greenAccent[500]} variant="h5">
+              {faq.summary}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{faq.details}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
+  );
+};
+
+export default FAQ;
